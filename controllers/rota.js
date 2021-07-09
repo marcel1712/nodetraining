3a6c19d1faee15fdcd5401b42b6cdc654893fa54
Marcel Henrique <marcelhrb@gmail.com>
2021-07-08 21:05:09 -0300

    translating data from client

--- controllers/rota.js
+++ controllers/rota.js
@@ -1,5 +1,7 @@
 module.exports = app =>{
     app.get('/atendimentos', (req,res)=> res.send(`Rota atendimentos com o GET`));
-
-    app.post('/atendimentos', (req,res)=> res.send(`Rota atendimentos com o POST`))
+    app.post('/atendimentos', (req,res)=> {
+        console.log(req.body)
+        res.send(`Rota atendimentos com o POST`)
+    })
 }
