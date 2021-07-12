be96ebf6857664f7547c99cca932e76d55eb0502
Marcel Henrique <marcelhrb@gmail.com>
2021-07-12 20:11:08 -0300

    connecting with database

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const customExpress = require('./config/customexpress');
-const app = customExpress();
+const conexao = require('./infraestrutura/conexao')
 
-app.listen(3000, ()=> console.log('Servidor trabalhando na porta 3000'));
+conexao.connect( erro => {
+    if(erro){
+        console.log(erro)
+    }else{
+        console.log('conectado com sucesso')
+        const app = customExpress();
+        app.listen(3000, ()=> console.log('Servidor trabalhando na porta 3000'));
+    }
+})
